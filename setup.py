ec9e71eb9e3779fdc4a5bb95a3b7c2457583b2df
Thanh Nguyen <thanh.nguyen@uclouvain.be>
2017-04-21 12:13:21 -0500

    fix(version): fix flask version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='flask_postgres_session',
     install_requires=[
-        "Flask==0.10.1",
+        "Flask==0.12.1",
         "psycopg2==2.6.1",
         "sqlalchemy==1.1.9",
         "Flask-SQLAlchemy-Session==1.1"
