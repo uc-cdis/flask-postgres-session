68bf5a9723a351729855c429eca8a0f4bbb830c7
Thanh Nguyen <thanhnd@uchicago.edu>
2017-12-13 14:33:41 -0600

    fix(psycopg): update version of psycopg

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='flask_postgres_session',
     install_requires=[
         "Flask==0.10.1",
-        "psycopg2==2.6.1",
+        "psycopg2>=2.7",
         "sqlalchemy==0.9.9",
         "Flask-SQLAlchemy-Session==1.1"
     ],
