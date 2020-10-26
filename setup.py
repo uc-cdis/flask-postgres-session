175b24d057f81ec21502c6f0e6d9e8ead3d3fa26
Pauline Ribeyre <ribeyre@uchicago.edu>
2020-10-26 15:38:08 -0500

    Add pre-commit config (#11)

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='flask_postgres_session',
+    name="flask_postgres_session",
     install_requires=[
         "Flask==0.10.1",
         "psycopg2>=2.7",
         "sqlalchemy==0.9.9",
-        "Flask-SQLAlchemy-Session==1.1"
+        "Flask-SQLAlchemy-Session==1.1",
     ],
     packages=find_packages(),
 )
